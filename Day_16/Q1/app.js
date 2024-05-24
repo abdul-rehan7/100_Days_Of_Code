46db0efd6ca02232108fa40f9f0ea79c585361d1
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-24 21:02:32 +0500

    day16 - Q2 and Q3 Done

--- Day_16/Q1/app.js
+++ Day_16/Q1/app.js
@@ -1,10 +1,11 @@
-"use strict";
+// Enhanced Laptop Object: Construct an object for a laptop including properties make, model, year, and a method describe() that logs a sentence about the laptop. 
 let laptop = {
     make: "Dell",
     model: "XPS 15",
     year: 2021,
-    explain: function () {
+    describe: function () {
         console.log(`This laptop is a ${this.year} ${this.make} ${this.model}.`);
     },
 };
-laptop.explain();
+laptop.describe();
+export {};
---
+++ Day_16/Q2/app.ts
@@ -0,0 +1,10 @@
+// Advanced Array Destructuring: Given an array of objects representing different laptops, each with properties make, model, and year, use array destructuring to assign the first and second laptops to variables. Then, log these variables.
+let laptops = [
+  { make: "Dell", model: "XPS 15", year: 2021 },
+  { make: "Apple", model: "MacBook Pro", year: 2020 },
+  { make: "HP", model: "Spectre x360", year: 2021 },
+];
+let [laptop1, laptop2, laptop3] = laptops;
+console.log(`This is a ${laptop1.make} ${laptop1.model} ${laptop1.year} Laptop`);
+console.log(`This is a ${laptop2.make} ${laptop2.model} ${laptop2.year} Laptop`);
+console.log(`This is a ${laptop3.make} ${laptop3.model} ${laptop3.year} Laptop`);
