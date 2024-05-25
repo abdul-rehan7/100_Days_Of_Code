704aa77173e5a6760edd8b9f649f778fa0fdef5f
rehanmirza76 <abdulrehan0317@gmail.com>
2024-05-25 14:22:28 +0500

    Day_16 - Q1,Q2,Q3 Done ☺️

--- Day_16/Q3/app.ts
+++ Day_16/Q3/app.ts
@@ -1,4 +1,4 @@
-// Combining Arrays with Spread Operator: Suppose you're comparing prices of two different sets of laptops. Use the spread operator to combine these arrays into a single array sorted in ascending order, then log the result. 
+// Combining Arrays with Spread Operator: Suppose you're comparing prices of two different sets of laptops.Use the spread operator to combine these arrays into a single array sorted in ascending order, then log the result. 
 
 let pricesSet1 = [1200, 1500, 1100];
 let pricesSet2 = [1000, 1300, 1600];
