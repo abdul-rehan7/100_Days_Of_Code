457ce677a9a4081dd5d44b2b286d6a78eafacb8c
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-01 16:21:10 +0500

    Day_22 - Q1,Q2,Q3 Done ☺️

---
+++ Day_22/Q1/app.js
@@ -0,0 +1,5 @@
+// Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example, if provided with "Age: " and 30, it should give back "Age: 30".
+function concatinate(a, b) {
+    return a + b;
+}
+export {};
---
+++ Day_22/Q2/app.ts
@@ -0,0 +1,8 @@
+// Determine the Remainder: Make a function that gets two numbers and shows the leftover from dividing them using the % sign. For example, remainder(5, 2) should give 1. 
+
+
+function showReminder(a:number,b:number){
+    return a%b;
+}
+
+console.log(`The Remainder is ${showReminder(12,5)}`);
