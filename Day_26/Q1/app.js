ff51a31d2f2f6705443b3270ec0e42d77b6cbbd0
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-22 16:55:39 +0500

    Day_26 - Q1,Q2,Q3 Done ☺️

---
+++ Day_26/Q1/app.js
@@ -0,0 +1,7 @@
+// Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together, and returns the result. Show how you can call this function and log the result. 
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+console.log(add(10, 5));
+export {};
---
+++ Day_26/Q1/app.ts
@@ -0,0 +1,8 @@
+// Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together, and returns the result. Show how you can call this function and log the result. 
+
+function add(a:number,b:number){
+    let sum = a + b
+    return sum
+}
+
+console.log(add(10,5))
