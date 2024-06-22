4df3208e5f7b2262dcfe9fd5a21be22d448d7762
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-22 17:01:22 +0500

    Day_26 - Q1,Q2,Q3 Done ☺️

--- Day_26/Q3/app.js
+++ Day_26/Q3/app.js
@@ -1,1 +1,10 @@
+// Function Expressions vs. Function Declarations: Compare function expressions and declarations by creating one of each that performs the same task, such as squaring a number.
+function squareDeclaration(number) {
+    return number * number;
+}
+const squareExpression = function (number) {
+    return number * number;
+};
+console.log(squareDeclaration(4));
+console.log(squareExpression(4));
 export {};
