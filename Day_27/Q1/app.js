355fe501889d7dcbf662dc9d1082b15776534bbe
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-23 13:58:07 +0500

    Day_27 - Q1,Q2,Q3 Done ☺️

---
+++ Day_27/Q1/app.js
@@ -0,0 +1,8 @@
+// Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car. 
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2024
+};
+console.log(`This a car made by ${car.make} and it's model is ${car.model} made in year ${car.year}`);
+export {};
---
+++ Day_27/Q2/app.js
@@ -0,0 +1,10 @@
+// Updating Object Properties: Add a property named `color` to the existing car object, and then update the `year` property to `2021`. Show how to perform these operations.
+let new_car = {
+    make: "Honda",
+    model: "Civic",
+    year: 2022,
+};
+new_car.year = 2024;
+new_car.color = "red";
+console.log(new_car);
+export {};
