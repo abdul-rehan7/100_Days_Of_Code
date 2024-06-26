3d8e76b15034af43718aa750cf557b984d7e0136
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-26 15:47:41 +0500

    Day_29 - Q1,Q2,Q3 Done ☺️

---
+++ Day_29/Q2/app.ts
@@ -0,0 +1,13 @@
+// Checking for Text Presence: Create a function that checks if the word `JavaScript` is present in a given string. It should return `true` if found, otherwise `false`.
+import inquirer from "inquirer";
+
+let ask = await inquirer.prompt([
+  { name: "Q1", type: "input", message: "Enter the Word to search..." },
+  { name: "Q2", type: "input", message: "Enter the Paragraph : " },
+]);
+
+function hasJavaScript(str: string): boolean {
+  return str.includes(ask.Q1);
+}
+
+console.log(hasJavaScript(ask.Q2))
---
+++ Day_29/Q3/app.js
@@ -0,0 +1,15 @@
+// Extracting a Substring: Demonstrate how to extract the first 10 characters from a string. 
+import inquirer from "inquirer";
+let ask = await inquirer.prompt([{
+        name: "Q1",
+        type: "input",
+        message: "Enter The starting position :"
+    }, {
+        name: "Q2",
+        type: "input",
+        message: "Enter The Ending position :"
+    }]);
+function extractFirstTenChars(str) {
+    return str.substring(ask.Q1, ask.Q2);
+}
+console.log(extractFirstTenChars("Hello, JavaScript world!"));
