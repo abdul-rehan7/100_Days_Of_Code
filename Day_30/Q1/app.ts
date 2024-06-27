77a613a4d67c28a96fb672315d2dd0f8747efdd1
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-27 20:21:52 +0500

    Day_30 - Q1,Q2,Q3 Done ☺️

---
+++ Day_30/Q1/app.ts
@@ -0,0 +1,6 @@
+// Converting Strings to Numbers: Write a function that takes a string representation of a number (e.g., `123`) and converts it into an actual number type.
+
+function convertStringToNumber(str: string): number {
+  return Number(str);
+}
+console.log(convertStringToNumber("123"));
