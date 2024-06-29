361f0b8ed78554d74096df613e0a3d9bd64b6328
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-29 16:25:41 +0500

    Day_31- Q1,Q2,Q3 Done ☺️

---
+++ Day_31/Q1/app.js
@@ -0,0 +1,5 @@
+// Create an array of your three favorite fruits and add a new fruit to the end of the array. 
+let fruits = ["Apple", "Mango", "Orange"];
+fruits.push("Grapes");
+console.log(fruits);
+export {};
---
+++ Day_31/Q1/app.ts
@@ -0,0 +1,6 @@
+// Create an array of your three favorite fruits and add a new fruit to the end of the array. 
+
+let fruits = ["Apple","Mango","Orange"]
+
+fruits.push("Grapes")
+console.log(fruits)
