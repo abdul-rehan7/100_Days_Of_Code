d8bf137a169399e0089d79cb8ca46ef1b2fff5c8
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-29 16:41:27 +0500

    Day_31 - Q1,Q2,Q3 Done ☺️

--- Day_31/Q3/app.js
+++ Day_31/Q3/app.js
@@ -1,1 +1,21 @@
-export {};
+// Find the index of `Banana` in an array of fruits and replace it with `Mango`.
+import inquirer from "inquirer";
+let fruits = ["Apple", "Orange", "Banana", "Grapes"];
+console.log(`\n\t This is the array of fruits 👉 ${fruits} \n`);
+let input = await inquirer.prompt([
+    {
+        name: "Q1",
+        type: "input",
+        message: "Enter the Fruit to be replaced : "
+    }, {
+        name: "Q2",
+        type: "input",
+        message: "Enter the Fruit to be Added : "
+    }
+]);
+function replaceFruit(fruit) {
+    let index = fruits.indexOf(input.Q1);
+    fruits[index] = fruit;
+    console.log(`\n\t This is the Modified array of fruits 👉 ${fruits}`);
+}
+replaceFruit(input.Q2);
