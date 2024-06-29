df9e684013d945b09d19d61b08820b4873f0d3e6
rehanmirza76 <abdulrehan0317@gmail.com>
2024-06-29 16:29:25 +0500

    Day_31 - Q1,Q2,Q3 Done ☺️

--- Day_31/Q2/app.js
+++ Day_31/Q2/app.js
@@ -1,1 +1,8 @@
+// Write a function to remove the last element from an array and return the removed element.
+let cars = ["Ferrari", "Bugatti", "Supra", "Lambo"];
+function removeLast(arr) {
+    arr.pop();
+    console.log(arr);
+}
+removeLast(cars);
 export {};
