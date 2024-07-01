6f4b7648ed4125a5eae9fa70f014c3e82ffe69a0
rehanmirza76 <abdulrehan0317@gmail.com>
2024-07-01 18:34:15 +0500

    Day_32 - Q1,Q2,Q3 Done ☺️

--- Day_32/Q2/app.ts
+++ Day_32/Q2/app.ts
@@ -1,6 +1,6 @@
 // Write a function that uses the .filter() method to return an array of numbers greater than 10. 
 
-let array = [9,12,5,17,20 ]
+let array = [9,12,5,17,20]
 
 let nums = array.filter((num) => num > 10)
-console.log(nums)
+console.log(nums) 
